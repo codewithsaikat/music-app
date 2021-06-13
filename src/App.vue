6b12e0707fd74b813cd9db95dea560f33a68808e
saikat hasan <saiakthasan441@gmail.com>
2021-06-13 16:28:19 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div id="app">
+    <header>
+        <h1>My Music</h1>
+    </header>
+    <main>
+        <section class="player">
+            <h2 class="song-title">{{current.title}} - <span>{{current.artist}}</span>
+            </h2>
+            <div class="control">
+              <button class="prev">Prev</button>
+              <button class="play" v-if="!isPlaying">Play</button>
+              <button class="pause" v-else>Pause</button>
+              <button class="next">Next</button>
+            </div>
+             
+
+        </section>
+
+    </main>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      current: {},
+      index: 0,
+      isPlaying: false,
+      songs: [ 
+        {
+          title: 'Grateful',
+          artist: 'Neffex',
+          src: require ('./assets/neffex-grateful.mp3')
+
+        },
+
+        {
+          title: 'Invincible',
+          artist: 'Deaf Kev',
+          src: require ('./assets/deaf-kev-invincible.mp3')
+        }
+        
+      ],
+      player: new Audio()
+    }
+  },
+  methods: {
+    play (song) {
+      if (typeof song.src != "undefined") {
+        this.curent = song;
+        this.player.src = this.current.src;
+      }
+
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause () {
+      this.player.pause();
+      this.isPlaying = false;
+    }
+  },
+
+  
+  created () {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+    //  
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  
 }
+
+body {
+  font-family: sans-serif;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #FFF;
+
+}
+
 </style>
